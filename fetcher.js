371f6dfbf07036cbec56e65603a85f04f408c1de
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-12-02 23:22:46 +0000

    Added conditions to check if the file already exits

--- fetcher.js
+++ fetcher.js
@@ -1,23 +1,61 @@
 const request = require('request');
 const fs = require('fs');
-const args = process.argv;
+const readline = require('readline');
+const { stdin } = require('process');
 
-//function to establish conection and write it in a file
-const fetchInformation = function (requestURL,fileToWrite){
-  request(requestURL,(error,response,body) => {
-    if(error){
-      console.log('Error: ',error);
-    } else {
-      fs.writeFile(fileToWrite,body,'utf-8',(error,bytes) => {
-        if(error){
-        console.log('Error');
-        } else {
-        const fileSize = fs.statSync(fileToWrite);
-        console.log(`Downloaded and saved ${fileSize.size} bytes to ${fileToWrite}`);
-        }
-      })
-    }
-  })
+// function to get the arguments 
+const getArguments = () =>{
+  const args = process.argv.slice(2);
+  if(args.length === 0) {
+    console.log('Please provide a valid URL and filename');
+    process.exit(1);
+  } else if(!args[1]){
+    console.log('Invalid pathname!');
+    process.exit();
+  } else {
+    return args;  
+  }
 };
 
-fetchInformation(args[2],args[3]);
+const [URL, destination] = getArguments();
+
+//Readline to create stdin and stdout
+const rl = readline.createInterface({
+  input:process.stdin,
+  output:process.stdout
+})
+
+//function to ask if the file has to be Overwritten
+const askToOverwrite = function(file,data){
+    rl.question('Do you want to overwrite? (y or n)',(answer) => {
+      if(answer.toLowerCase() === 'y'){
+        storeInformation(file,data);
+      }else if(answer.toLowerCase() === 'n'){
+        process.exit();
+      } else {
+        storeInformation(file, data);
+      }
+    })
+}
+//establishes connections and passes the data 
+request(URL,(error,response,body) => {
+    if(error){
+      console.log('Error: ',error);
+    } else if(fs.existsSync(destination)) {
+      askToOverwrite(destination,body);      
+    } else {
+      storeInformation(destination,body);      
+    }
+  })
+
+const storeInformation = function(fileToWrite,body){
+  fs.writeFile(fileToWrite,body,'utf-8',error => {
+    if(error){
+    console.log('Error');
+    } else {
+    const fileSize = fs.statSync(fileToWrite);
+    console.log(`Downloaded and saved ${fileSize.size} bytes to ${fileToWrite}`);
+    rl.close();
+    }
+  })
+}
